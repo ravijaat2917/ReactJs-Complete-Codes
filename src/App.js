dacaaa1a45fb71b40bae6ea071152b273db968ae
Ravi Lour <ravijaat2917@gmail.com>
2023-04-02 16:01:12 +0530

    Code Upto day 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import './App.css';
+import Video from "./components/video";
+import "./App.css";
+import data from "./data/data.js";
 
-function App() {
+let videos = data;
+
+let App = () => {
   return (
-    <div className="App">
-      <div className='App-header'>
-        Hello World...
+    <>
+      <h4>Videos</h4>
+
+      <div className="row">
+
+        {
+          videos.map(video =><div className="video"><Video
+            channel={video.channel }
+            varified={video.varified}
+            time={video.time}
+            views={video.views}
+            title={video.title}
+            imglink={video.imglink} ></Video> </div>
+          )
+        }
+
+        <div className="video">
+          <Video
+            channel="Coder Dost"
+            varified={false}
+            time="1 month ago"
+            views="10k"
+            title="ReactJs-Tutorial Thumbnail"
+            imglink="https://source.unsplash.com/random/220x150"
+          ></Video>
+        </div>
+
+        <div className="video">
+          <Video
+            channel="Geeky Show"
+            time="1 year ago"
+            varified={true}
+            views="1.2M"
+            title="NodeJs-Tutorial Thumbnail"
+            imglink="https://picsum.photos/220/150?random=1"
+          ></Video>
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+const data =  [
+    {
+      channel: "Coder Dost",
+      varified: false,
+      time: "3 month ago",
+      views: "10k",
+      title: "Flutter-Tutorial Thumbnail",
+      imglink: "https://picsum.photos/220/150?random=2",
+    },
+    {
+      channel: "Coder Dost",
+      varified: true,
+      time: "10 month ago",
+      views: "10k",
+      title: "ExpressJs-Tutorial Thumbnail",
+      imglink: "https://picsum.photos/220/150?random=3",
+    },
+    {
+      channel: "Coder Dost",
+      varified: false,
+      time: "2 month ago",
+      views: "10k",
+      title: "ReactJs-Tutorial Thumbnail",
+      imglink: "https://picsum.photos/220/150?random=4",
+    },
+    {
+      channel: "Coder Dost",
+      varified: false,
+      time: "1 year ago",
+      views: "10k",
+      title: "MongoDB-Tutorial Thumbnail",
+      imglink: "https://picsum.photos/220/150?random=5",
+    },
+  ];
+  export default data ;
